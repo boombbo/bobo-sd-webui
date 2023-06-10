f30c4e6d92f5a406df72218aa42079f7d9539d38
boombbo <102649624+boombbo@users.noreply.github.com>
2023-06-10 17:42:18 +0800

    Initialize user/vemancha/ToastProgressData_L2

---
+++ dev/ToastNotifications/ToastProgressData.cpp
@@ -0,0 +1,47 @@
+﻿#include "pch.h"
+#include "ToastProgressData.h"
+#include "Microsoft.Windows.ToastNotifications.ToastProgressData.g.cpp"
+
+namespace winrt::Microsoft::Windows::ToastNotifications::implementation
+{
+    uint32_t ToastProgressData::SequenceNumber()
+    {
+        return m_sequenceNumber;
+    }
+    void ToastProgressData::SequenceNumber(uint32_t value)
+    {
+        m_sequenceNumber = value;
+    }
+    hstring ToastProgressData::Title()
+    {
+        return m_title;
+    }
+    void ToastProgressData::Title(hstring const& value)
+    {
+        m_title = value;
+    }
+    double ToastProgressData::Value()
+    {
+        return m_progressValue;
+    }
+    void ToastProgressData::Value(double value)
+    {
+        m_progressValue = value;
+    }
+    hstring ToastProgressData::ValueStringOverride()
+    {
+        return m_progressvalueString;
+    }
+    void ToastProgressData::ValueStringOverride(hstring const& value)
+    {
+        m_progressvalueString = value;
+    }
+    hstring ToastProgressData::Status()
+    {
+        return m_progressStatus;
+    }
+    void ToastProgressData::Status(hstring const& value)
+    {
+        m_progressStatus = value;
+    }
+}
---
+++ dev/ToastNotifications/ToastProgressData.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+#include "Microsoft.Windows.ToastNotifications.ToastProgressData.g.h"
+
+namespace winrt::Microsoft::Windows::ToastNotifications::implementation
+{
+    struct ToastProgressData : ToastProgressDataT<ToastProgressData>
+    {
+        ToastProgressData() = default;
+
+        uint32_t SequenceNumber();
+        void SequenceNumber(uint32_t value);
+        hstring Title();
+        void Title(hstring const& value);
+        double Value();
+        void Value(double value);
+        hstring ValueStringOverride();
+        void ValueStringOverride(hstring const& value);
+        hstring Status();
+        void Status(hstring const& value);
+
+    private:
+        uint32_t m_sequenceNumber;
+        hstring m_title;
+        double m_progressValue;
+        hstring m_progressvalueString;
+        hstring m_progressStatus;
+    };
+}
+namespace winrt::Microsoft::Windows::ToastNotifications::factory_implementation
+{
+    struct ToastProgressData : ToastProgressDataT<ToastProgressData, implementation::ToastProgressData>
+    {
+    };
+}
