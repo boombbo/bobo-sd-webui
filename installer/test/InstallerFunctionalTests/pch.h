98d3631a01671a7d020c6845451d0df0d4eb014b
boombbo <102649624+boombbo@users.noreply.github.com>
2023-06-10 17:41:49 +0800

    Initialize andreww-msft-patch-3

---
+++ installer/test/InstallerFunctionalTests/pch.h
@@ -0,0 +1,29 @@
+﻿// pch.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add files here that you will be updating frequently as this negates the performance advantage.
+
+#ifndef PCH_H
+#define PCH_H
+
+#include <windows.h>
+#include <wil/result.h>
+#include <wil/cppwinrt.h>
+#include <wil/resource.h>
+#include <wil/token_helpers.h>
+#include <wil/win32_helpers.h>
+#include <wil/result_macros.h>
+
+#include <string>
+#include <sstream>
+#include <filesystem>
+
+#include <WexTestClass.h>
+
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Management.Deployment.h>
+#include <winrt/Windows.System.h>
+
+#endif //PCH_H
