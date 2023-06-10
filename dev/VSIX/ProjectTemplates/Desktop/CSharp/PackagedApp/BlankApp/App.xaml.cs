bf312934bc246c0e5092a685069736d8242b6703
boombbo <102649624+boombbo@users.noreply.github.com>
2023-06-10 17:42:15 +0800

    Initialize add-cswin32

---
+++ dev/VSIX/ProjectTemplates/Desktop/CSharp/PackagedApp/BlankApp/App.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using Microsoft.UI.Xaml.Shapes;
+using static Windows.Win32.PInvoke;
+using Windows.Win32.Foundation;
+using Windows.Win32.UI.WindowsAndMessaging;
+using WinRT;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application
+    {
+        private Window m_window;
+        private HWND m_hwnd;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            m_window = new MainWindow();
+
+            m_hwnd = (HWND)WinRT.Interop.WindowNative.GetWindowHandle(m_window);
+
+            m_window.Activate();
+
+            SetWindowSize(m_hwnd, 800, 600);
+        }
+
+        /// <summary>
+        /// Uses Win32 interop to set the window size
+        /// </summary>
+        /// <param name="hwnd">Window handle</param>
+        /// <param name="width">Window width</param>
+        /// <param name="height">Window height</param>
+        private static void SetWindowSize(HWND hwnd, int width, int height)
+        {
+            var dpi = GetDpiForWindow(hwnd);
+            float scalingFactor = (float)dpi / 96f;
+            width = (int)(width * scalingFactor);
+            height = (int)(height * scalingFactor);
+
+            SetWindowPos(
+                hwnd, 
+                HWND_TOP,
+                0, 0, width, height,
+                SET_WINDOW_POS_FLAGS.SWP_NOMOVE);
+        }
+    }
+}
