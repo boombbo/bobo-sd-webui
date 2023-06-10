c7d738d7533a581c17cd558ce47004743d891907
boombbo <102649624+boombbo@users.noreply.github.com>
2023-06-10 17:37:04 +0800

    Initial commit

---
+++ dev/PushNotifications/PushNotificationsLongRunningTask.StartupTask/winmain.cpp
@@ -0,0 +1,14 @@
+﻿#include <windows.h>
+#include "pch.h"
+
+#include "PushNotificationsLongRunningPlatform-Startup.h"
+
+int WINAPI WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, PSTR /*lpCmdLine*/, int /*nCmdShow*/)
+{
+    RETURN_IF_FAILED(::CoInitializeEx(nullptr, COINITBASE_MULTITHREADED));
+
+    auto scopeExit = wil::scope_exit(
+        [&]() { CoUninitialize(); });
+
+    return StartupNotificationsLongRunningPlatform();
+}
---
+++ installer/dev/console.h
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include "pch.h"
+
+namespace WindowsAppRuntimeInstaller::Console
+{
+    void DisplayHeader();
+    void DisplayHelp();
+    void DisplayInfo();
+    void DisplayError(const HRESULT hr);
+}
