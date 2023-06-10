3415a16d5801b495d731dcf0eee0968da74e2b07
boombbo <102649624+boombbo@users.noreply.github.com>
2023-06-10 17:39:05 +0800

    Initialize user/benkuhn/drop-08-applifecycle

---
+++ build/CopyFilesToStagingDir.ps1
@@ -0,0 +1,48 @@
+[CmdLetBinding()]
+Param(
+    [string]$BuildOutputDir,
+    [string]$OverrideDir,
+    [string]$PublishDir,
+    [string]$NugetDir,
+    [string]$Platform,
+    [string]$Configuration,
+    [switch]$PublishAppxFiles=$false
+)
+
+$FullBuildOutput = "$($BuildOutputDir)\$($Configuration)\$($Platform)"
+$FullPublishDir = "$($PublishDir)\$($Configuration)\$($Platform)"
+
+if (!(Test-Path $FullPublishDir)) { mkdir $FullPublishDir }
+
+
+function PublishFile {
+    Param($source, $destinationDir, [switch]$IfExists = $false)
+
+    if ((-not $IfExists) -or (Test-Path $source))
+    {
+        Write-Host "Copy from '$source' to '$destinationDir'"
+        if (-not (Test-Path $destinationDir))
+        {
+            $ignore = New-Item -ItemType Directory $destinationDir
+        }
+        Copy-Item -Recurse -Force $source $destinationDir
+    }
+    else
+    {
+        Write-Host "Not copying '$source' to $destinationDir because it did not exist"
+    }
+}
+
+PublishFile $OverrideDir\DynamicDependency-Override.json $FullPublishDir\
+
+PublishFile -IfExists $FullBuildOutput\FrameworkPackage\*.* $FullPublishDir\FrameworkPackage
+
+if($PublishAppxFiles)
+{
+    $AppxPackagesDir = "$FullPublishDir\AppxPackages"
+}
+
+# Copy files to Full Nuget package
+#
+# Dynamic Dependency build overrides
+PublishFile $OverrideDir\DynamicDependency-Override.json $NugetDir\runtimes\win10-$Platform\native
