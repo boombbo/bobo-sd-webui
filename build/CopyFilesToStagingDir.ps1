22fb1010e82a57dc848337a23d87cd5a7b64457d
boombbo <102649624+boombbo@users.noreply.github.com>
2023-06-10 17:41:36 +0800

    Initialize release/reunion-0.8-perf

---
+++ build/CopyFilesToStagingDir.ps1
@@ -0,0 +1,43 @@
+[CmdLetBinding()]
+Param(
+    [string]$BuildOutputDir,
+    [string]$OverrideDir,
+    [string]$PublishDir,
+    [string]$NugetDir,
+    [string]$Platform,
+    [string]$Configuration,
+    [switch]$PublishAppxFiles=$false
+)
+
+$FullBuildOutput = "$($BuildOutputDir)\$($Configuration)\$($Platform)"
+$FullPublishDir = "$($PublishDir)\$($Configuration)\$($Platform)"
+
+if (!(Test-Path $FullPublishDir)) { mkdir $FullPublishDir }
+if (!(Test-Path $NugetDir)) { mkdir $NugetDir }
+
+function PublishFile {
+    Param($source, $destinationDir, [switch]$IfExists = $false)
+
+    if ((-not $IfExists) -or (Test-Path $source))
+    {
+        Write-Host "Copy from '$source' to '$destinationDir'"
+        if (-not (Test-Path $destinationDir))
+        {
+            $ignore = New-Item -ItemType Directory $destinationDir
+        }
+        Copy-Item -Recurse -Force $source $destinationDir
+    }
+    else
+    {
+        Write-Host "Not copying '$source' to $destinationDir because it did not exist"
+    }
+}
+
+PublishFile $OverrideDir\DynamicDependency-Override.json $FullPublishDir\
+
+PublishFile -IfExists $FullBuildOutput\FrameworkPackage\*.* $FullPublishDir\FrameworkPackage
+
+if($PublishAppxFiles)
+{
+    $AppxPackagesDir = "$FullPublishDir\AppxPackages"
+}
