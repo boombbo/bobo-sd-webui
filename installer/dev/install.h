98021eea569462dbb2d0d2945845cb1b8744677e
boombbo <102649624+boombbo@users.noreply.github.com>
2023-06-10 17:44:08 +0800

    Initialize user/drustheaxe/bootstrap-initialize2-spec

---
+++ installer/dev/install.h
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+
+#include "pch.h"
+#include "packages.h"
+
+namespace WindowsAppRuntimeInstaller {
+
+    enum class Options
+    {
+        InstallPackages     = 0x0001,
+        InstallLicenses     = 0x0002,
+        Quiet               = 0x0004,
+        DryRun              = 0x0008,
+        ForceDeployment     = 0x0010,
+    };
+    DEFINE_ENUM_FLAG_OPERATORS(Options)
+
+    struct PackageProperties
+    {
+        wil::unique_cotaskmem_string fullName;
+        wil::unique_cotaskmem_string familyName;
+        winrt::Windows::System::ProcessorArchitecture architecture{ winrt::Windows::System::ProcessorArchitecture::Unknown };
+        std::uint64_t version{};
+        bool isFramework{};
+    };
+
+    HRESULT Deploy(const WindowsAppRuntimeInstaller::Options options) noexcept;
+
+    HRESULT InstallLicenses(const WindowsAppRuntimeInstaller::Options options);
+
+    HRESULT DeployPackages(const WindowsAppRuntimeInstaller::Options options);
+
+}
