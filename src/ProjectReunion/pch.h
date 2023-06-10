ce4456f153aa67f6e84ac2d883ff5e7635f5bc54
boombbo <102649624+boombbo@users.noreply.github.com>
2023-06-10 17:37:05 +0800

    Initialize user/howardk/m.am.package

---
+++ src/ProjectReunion/pch.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+// Temporary hack until switching to the ProjectReunion project @jonwis is cooking
+#if defined(WINAPI_FAMILY)
+#   undef WINAPI_FAMILY
+#endif
+
+#include "targetver.h"
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Foundation.Metadata.h>
+
+#include <winrt/Windows.ApplicationModel.h>
+#include <winrt/Windows.Data.Xml.Dom.h>
+#include <winrt/Windows.Storage.Streams.h>
+#include <winrt/Windows.Management.Deployment.h>
