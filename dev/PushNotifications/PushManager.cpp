39ef18182f81cd75a35ecf8cadec440bbfe988a9
boombbo <102649624+boombbo@users.noreply.github.com>
2023-06-10 17:37:49 +0800

    Initialize user/joayalag/bta-stubs

---
+++ dev/PushNotifications/PushManager.cpp
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#include "pch.h"
+#include "PushManager.h"
+#include "PushManager.g.cpp"
+
+namespace winrt::Microsoft::ProjectReunion::implementation
+{
+    void PushManager::RegisterPushNotificationActivator(Microsoft::ProjectReunion::InProcActivatorDetails const& details)
+    {
+        throw hresult_not_implemented();
+    }
+    void PushManager::UnregisterPushNotificationActivator(Windows::ApplicationModel::Background::IBackgroundTaskRegistration const& registration)
+    {
+        throw hresult_not_implemented();
+    }
+    Windows::Foundation::IAsyncOperationWithProgress<Microsoft::ProjectReunion::ChannelResult, Microsoft::ProjectReunion::ChannelResult> PushManager::CreateChannelAsync(winrt::guid remoteId)
+    {
+        throw hresult_not_implemented();
+    }
+}
---
+++ dev/PushNotifications/PushManager.h
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+#include "PushManager.g.h"
+
+namespace winrt::Microsoft::ProjectReunion::implementation
+{
+    struct PushManager
+    {
+        PushManager() = default;
+
+        static void RegisterPushNotificationActivator(Microsoft::ProjectReunion::InProcActivatorDetails const& details);
+        static void UnregisterPushNotificationActivator(Windows::ApplicationModel::Background::IBackgroundTaskRegistration const& registration);
+        static Windows::Foundation::IAsyncOperationWithProgress<Microsoft::ProjectReunion::ChannelResult, Microsoft::ProjectReunion::ChannelResult> CreateChannelAsync(winrt::guid remoteId);
+    };
+}
+namespace winrt::Microsoft::ProjectReunion::factory_implementation
+{
+    struct PushManager : PushManagerT<PushManager, implementation::PushManager>
+    {
+    };
+}
