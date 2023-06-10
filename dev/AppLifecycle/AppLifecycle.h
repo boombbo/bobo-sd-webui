d7a347141b9c6b99ee3d9022638ae7ab38da7d1e
boombbo <102649624+boombbo@users.noreply.github.com>
2023-06-10 17:38:02 +0800

    Initialize user/joayalag/ba-impl

---
+++ dev/AppLifecycle/AppLifecycle.h
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+#pragma once
+
+#include <Microsoft.ApplicationModel.Activation.AppLifecycle.g.h>
+
+wil::unique_handle g_waitHandleForArgs = nullptr;
+winrt::Microsoft::ProjectReunion::PushNotificationReceivedEventArgs g_activatedEventArgs{ nullptr };
+
+namespace winrt::Microsoft::ApplicationModel::Activation::implementation
+{
+    struct AppLifecycle
+    {
+        AppLifecycle() = default;
+
+        static Windows::ApplicationModel::Activation::IActivatedEventArgs GetActivatedEventArgs();
+
+        static Microsoft::ApplicationModel::Activation::ActivationArguments GetActivatedEventArgs2();
+    };
+}
+
+namespace winrt::Microsoft::ApplicationModel::Activation::factory_implementation
+{
+    struct AppLifecycle : AppLifecycleT<AppLifecycle, implementation::AppLifecycle>
+    {
+    };
+}
