7a7c82c8496a7cecd4f160b034a6c78fed80a09a
boombbo <102649624+boombbo@users.noreply.github.com>
2023-06-10 17:39:35 +0800

    Initialize user/purifoypaul/PushNotificationsRename

---
+++ dev/AppLifecycle/LaunchActivatedEventArgs.h
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+#pragma once
+
+#include "ActivatedEventArgsBase.h"
+#include "ValueMarshaling.h"
+
+namespace winrt::Microsoft::Windows::AppLifecycle::implementation
+{
+    using namespace winrt::Windows::ApplicationModel::Activation;
+
+    constexpr PCWSTR c_launchContractId = L"Windows.Launch";
+
+    class LaunchActivatedEventArgs : public winrt::implements<LaunchActivatedEventArgs, ILaunchActivatedEventArgs,
+        ActivatedEventArgsBase, IInternalValueMarshalable>
+    {
+    public:
+        LaunchActivatedEventArgs(const std::wstring& args) : m_args(std::move(args))
+        {
+            m_kind = ActivationKind::Launch;
+        }
+
+        static winrt::Windows::Foundation::IInspectable Deserialize(winrt::Windows::Foundation::Uri const& uri)
+        {
+            auto query = uri.QueryParsed();
+            auto args = std::wstring(query.GetFirstValueByName(L"Arguments"));
+            return make<LaunchActivatedEventArgs>(args);
+        }
+
+        // IInternalValueMarshalable
+        winrt::Windows::Foundation::Uri Serialize()
+        {
+            auto uri = GenerateEncodedLaunchUri(L"App", c_launchContractId) + L"&Arguments=" + m_args;
+            return winrt::Windows::Foundation::Uri(uri);
+        }
+
+        // ILaunchActivatedEventArgs
+        winrt::hstring Arguments()
+        {
+            return m_args.c_str();
+        }
+
+        winrt::hstring TileId()
+        {
+            // This implementation is only used for Win32 which don't support secondary tiles.
+            return L"";
+        }
+
+    private:
+        std::wstring m_args;
+    };
+}
---
+++ dev/PushNotifications/PushNotificationChannel.h
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+#include "Microsoft.WindowsApp.PushNotifications.PushNotificationChannel.g.h"
+
+namespace winrt::Microsoft::WindowsApp::PushNotifications::implementation
+{
+    struct PushNotificationChannel : PushNotificationChannelT<PushNotificationChannel>
+    {
+        PushNotificationChannel(winrt::Windows::Networking::PushNotifications::PushNotificationChannel const& channel);
+        winrt::Windows::Foundation::Uri Uri();
+        winrt::Windows::Foundation::DateTime ExpirationTime();
+        void Close();
+
+        winrt::event_token PushReceived(winrt::Windows::Foundation::TypedEventHandler<Microsoft::WindowsApp::PushNotifications::PushNotificationChannel, Microsoft::Windows::PushNotifications::PushNotificationReceivedEventArgs> handler);
+        void PushReceived(winrt::event_token const& token) noexcept;
+
+    private:
+        const winrt::Windows::Networking::PushNotifications::PushNotificationChannel m_channel{ nullptr };
+
+    };
+}
+namespace winrt::Microsoft::WindowsApp::PushNotifications::factory_implementation
+{
+    struct PushNotificationChannel : PushNotificationChannelT<PushNotificationChannel, implementation::PushNotificationChannel>
+    {
+    };
+}
