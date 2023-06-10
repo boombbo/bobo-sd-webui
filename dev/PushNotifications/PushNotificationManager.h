cae17d2d3cc0b692a6869225940b25e5080f1c6d
boombbo <102649624+boombbo@users.noreply.github.com>
2023-06-10 17:40:01 +0800

    Initialize user/purifoypaul/ForegroundSinkRebase

---
+++ dev/PushNotifications/PushNotificationManager.h
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+#pragma once
+#include "Microsoft.Windows.PushNotifications.PushNotificationManager.g.h"
+
+namespace winrt::Microsoft::Windows::PushNotifications::implementation
+{
+    struct PushNotificationManager
+    {
+        PushNotificationManager() = delete;
+
+        static Microsoft::Windows::PushNotifications::PushNotificationRegistrationToken RegisterActivator(Microsoft::Windows::PushNotifications::PushNotificationActivationInfo const& details);
+        static void UnregisterActivator(Microsoft::Windows::PushNotifications::PushNotificationRegistrationToken const& token, Microsoft::Windows::PushNotifications::PushNotificationRegistrationOptions const& options);
+
+        static winrt::Windows::Foundation::IAsyncOperationWithProgress<winrt::Microsoft::Windows::PushNotifications::PushNotificationCreateChannelResult, winrt::Microsoft::Windows::PushNotifications::PushNotificationCreateChannelStatus> CreateChannelAsync(const winrt::guid &remoteId);
+
+        static uint32_t GetStringLengthFromComServer(const winrt::hstring& inputString);
+
+        static uint32_t TestForegroundSink();
+
+        static uint32_t ShutdownPlatformManually();
+
+    private:
+        static bool IsChannelRequestRetryable(const winrt::hresult& hrException);
+    };
+}
+namespace winrt::Microsoft::Windows::PushNotifications::factory_implementation
+{
+    struct PushNotificationManager : PushNotificationManagerT<PushNotificationManager, implementation::PushNotificationManager>
+    {
+    };
+}
